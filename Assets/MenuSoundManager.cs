4ace1e1f2e9c2084fb664e2223d257c9b06cb76a
Adrytekk <adryan.hauton@gmail.com>
2022-01-26 10:31:52 +0100

    MenuSound

---
+++ Assets/MenuSoundManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MenuSoundManager : MonoBehaviour
+{
+
+    public AudioMixer Musics;
+    public Slider slider;
+
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("CurrentVol");
+    }
+
+    public void SetMusicVol(float sliderValue)
+    {
+        Musics.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("CurrentVol", slider.value);
+    }
+}
